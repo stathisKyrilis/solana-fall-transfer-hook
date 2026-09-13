c7e8ec833557185a3772f7e1495ee79bd5d16ee7
stathiskyrilis <stathis1978@gmail.com>
2026-09-13 10:36:01 +0300

    challenge 3

--- programs/solana-fall-transfer-hook/src/instructions/transfer_hook.rs
+++ programs/solana-fall-transfer-hook/src/instructions/transfer_hook.rs
@@ -21,7 +21,7 @@ pub struct TransferHook<'info> {
     pub owner: UncheckedAccount<'info>,
     /// CHECK: ExtraAccountMetaList Account
     #[account(
-        seeds = [b"extra-account-metas", mint.key().as_ref()], 
+        seeds = [b"extra-account-metas", mint.key().as_ref()],
         bump
     )]
     pub extra_account_meta_list: UncheckedAccount<'info>,
@@ -29,7 +29,7 @@ pub struct TransferHook<'info> {
         mut,
         // Unique, program-wide rate limit account. See the CHALLENGE note in
         // `init_extra_account_meta.rs` for making this per-mint/per-owner.
-        seeds = [b"rate_limit"],
+        seeds = [b"rate_limit", mint.key().as_ref(), owner.key().as_ref()],
         bump,
     )]
     pub rate_limit: Account<'info, RateLimit>,
